4ab404cf43828fb37026cc41a633bb67bf818b36
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-10-04 23:29:05 +0200

    huh

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
-import studentsRouter from './routers/students.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
