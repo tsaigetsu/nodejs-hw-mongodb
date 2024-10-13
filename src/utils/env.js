6bd529c4de322ee5fcc9aba2c7454cff519d2ad6
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-10-13 16:54:59 +0200

    lol

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,22 @@
+//src/utils/env.js
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const env = (key, defaultValue = undefined) => {
   return process.env[key] || defaultValue;
+};
+
+export const getMongoDbUrl = () => {
+  const user = env('MONGODB_USER');
+  const password = env('MONGODB_PASSWORD');
+  const clusterUrl = env('MONGODB_URL');
+  const dbName = env('MONGODB_DB');
+
+  if (!user || !password || !clusterUrl || !dbName) {
+    throw new Error('MongoDB connection details are missing in environment variables');
+  }
+
+  return `mongodb+srv://${user}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority`;
 };
