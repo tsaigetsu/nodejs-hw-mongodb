c781f92acdac13eff19dce6c4ed47e5c78ef443f
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-10-17 18:40:01 +0200

    some updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 // src/index.js
+// src/index.js
 
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { initMongoConnections } from './db/initMongoConnections.js';
+import authRouter from './routers/auth.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { env } from './utils/env.js';
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth', authRouter);
+app.use('/contacts', contactsRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const startServer = async () => {
+  try {
+    await initMongoConnections();
+    const PORT = Number(env('PORT', 3000));
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 };
-bootstrap();
+
+startServer();
