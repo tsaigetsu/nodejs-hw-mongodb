ce2a78275085e6a1735ce0f5eaf2d08901af717c
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-10-21 20:22:11 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { initMongoConnections } from './db/initMongoConnections.js';
-import authRouter from './routers/auth.js';
+//import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
@@ -15,8 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRouter);
-app.use('/api/contacts', contactsRouter);
+//app.use('/auth', authRouter);
+app.use('/contacts', contactsRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
