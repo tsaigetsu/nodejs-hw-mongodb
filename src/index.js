1b1400646d88c7dd78242c9ef159e695d2306030
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-10-26 19:31:10 +0200

    okay

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { initMongoConnections } from './db/initMongoConnections.js';
-//import authRouter from './routers/auth.js';
+import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { env } from './utils/env.js';
-
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 const app = express();
+
+// Загружаем документ OpenAPI из YAML файла
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
 app.use(express.json());
 app.use(cookieParser());
 
-//app.use('/auth', authRouter);
+// Настройка Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Подключение роутов
+app.use('/auth', authRouter);
 app.use('/contacts', contactsRouter);
 
+// Обработчики ошибок
 app.use(notFoundHandler);
 app.use(errorHandler);
 
@@ -31,4 +40,5 @@ const startServer = async () => {
   }
 };
 
+// Запуск сервера
 startServer();
